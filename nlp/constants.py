b3f54cfebd6b2ffdc2b5047f23b9db49e5ffe3ad
lianet <lianet@datamaran.com>
2020-01-24 18:05:10 +0100

    extract subject, main verb and object of a sentence using spaCy

---
+++ nlp/constants.py
@@ -0,0 +1,15 @@
+terminology = {
+    "CstSec": [
+        [{'LOWER':'data'}, {'LOWER':'breach'}],
+        [{'LOWER':'data'}, {'LOWER':'protection'}],
+        [{'LOWER':'personal'}, {'LOWER':'information'}]
+    ]
+    ,
+    "IntelPty": [
+        [{'LOWER':'trade'}, {'LOWER':'secrets'}]
+    ]
+    ,
+    "HuRgts": [
+        [{'LOWER':'discrimination'}]
+    ]
+}
